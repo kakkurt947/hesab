dbe47454e74086b3de11fc320cb59a85d4d45b2a
MONU-GAMER <84650793+MONU-GAMER@users.noreply.github.com>
2022-03-08 18:17:34 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 def start() -> scoped_session:
-    engine = create_engine("postgres://visrynupdovxwp:906d6e3f0995150173028f03e64002a4cb14fb0a0308301fa6b23b82fe764621@ec2-3-230-238-86.compute-1.amazonaws.com:5432/d63r3fugndsia5", client_encoding="utf8")
+    engine = create_engine("postgres://ydjvszfi:yYiNC1As-vjwKzXCcr-X8Q82KD9Djxj3@ziggy.db.elephantsql.com/ydjvszfi", client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
